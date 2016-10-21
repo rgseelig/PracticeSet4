514072ef1b6a4646746e21d3dd33d904a6f5f5bb
Gavin <Gavin@DESKTOP-Q7HK301.fios-router.home>
2016-10-20 22:01:48 -0400

    PS4

--- src/ch/makery/address/view/PersonOverviewController.java
+++ src/ch/makery/address/view/PersonOverviewController.java
@@ -96,4 +96,20 @@ public class PersonOverviewController {
     		birthdayLabel.setText("");
     	}
     }
+    @FXML
+    private void handleDeletePerson() {
+        int selectedIndex = personTable.getSelectionModel().getSelectedIndex();
+        if (selectedIndex >= 0) {
+            personTable.getItems().remove(selectedIndex);
+        } else {
+            // Nothing selected.
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.initOwner(mainApp.getPrimaryStage());
+            alert.setTitle("No Selection");
+            alert.setHeaderText("No Person Selected");
+            alert.setContentText("Please select a person in the table.");
+
+            alert.showAndWait();
+        }
+    }
 }
